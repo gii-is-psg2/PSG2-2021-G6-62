2bb2cc43cfe6969ab92dd5378a6110a520ea1cf5
Jotaeme55 <Usuario@DESKTOP-I0NS17G.home>
2021-03-12 17:06:31 +0100

    cambiado mejorado

---
+++ src/main/java/org/springframework/samples/petclinic/model/PetHotel.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "hotel")
+public class PetHotel extends BaseEntity{
+
+	@Column(name = "address")
+	@NotEmpty
+	private String address;
+
+	@Column(name = "city")
+	@NotEmpty
+	private String city;
+
+	@Column(name = "telephone")
+	@NotEmpty
+	@Digits(fraction = 0, integer = 10)
+	private String telephone;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	private Set<Pet> pets;
+	
+}
